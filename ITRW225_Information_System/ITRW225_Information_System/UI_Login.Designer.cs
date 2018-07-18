80718546f756e5cf90472977fbcce531ab95bea7
coenraadhuman <coen.human@gmail.com>
2018-07-18 12:47:52 +0200

    Added Login UI and Parent UI

---
+++ ITRW225_Information_System/ITRW225_Information_System/UI_Login.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ITRW225_Information_System
+{
+    partial class UI_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxUser = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelForgot = new System.Windows.Forms.Label();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxUser
+            // 
+            this.textBoxUser.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxUser.Location = new System.Drawing.Point(183, 289);
+            this.textBoxUser.Name = "textBoxUser";
+            this.textBoxUser.Size = new System.Drawing.Size(206, 20);
+            this.textBoxUser.TabIndex = 0;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(183, 315);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.PasswordChar = '*';
+            this.textBoxPassword.Size = new System.Drawing.Size(206, 20);
+            this.textBoxPassword.TabIndex = 1;
+            // 
+            // labelForgot
+            // 
+            this.labelForgot.AutoSize = true;
+            this.labelForgot.Location = new System.Drawing.Point(242, 338);
+            this.labelForgot.Name = "labelForgot";
+            this.labelForgot.Size = new System.Drawing.Size(92, 13);
+            this.labelForgot.TabIndex = 2;
+            this.labelForgot.Text = "Forgot Password?";
+            this.labelForgot.Click += new System.EventHandler(this.labelForgot_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Location = new System.Drawing.Point(180, 260);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(0, 13);
+            this.labelStatus.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(565, 231);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // UI_Login
+            // 
+            this.AccessibleRole = System.Windows.Forms.AccessibleRole.OutlineButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(589, 370);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelForgot);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUser);
+            this.Name = "UI_Login";
+            this.Text = "Mr Salad - Login";
+            this.Load += new System.EventHandler(this.UI_Login_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxUser;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelForgot;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ ITRW225_Information_System/ITRW225_Information_System/UI_MainWindow.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace ITRW225_Information_System
+{
+    partial class UI_MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clientMaintenanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.employeeMaintenanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.userMaintenanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateClientToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addEmployeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateEmployeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeEmployeeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.updateUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.removeUserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.reportsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pointsOfSaleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.logoutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.logoutToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1264, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clientMaintenanceToolStripMenuItem,
+            this.employeeMaintenanceToolStripMenuItem,
+            this.pointsOfSaleToolStripMenuItem,
+            this.reportsToolStripMenuItem,
+            this.userMaintenanceToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // clientMaintenanceToolStripMenuItem
+            // 
+            this.clientMaintenanceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addClientToolStripMenuItem,
+            this.updateClientToolStripMenuItem});
+            this.clientMaintenanceToolStripMenuItem.Name = "clientMaintenanceToolStripMenuItem";
+            this.clientMaintenanceToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.clientMaintenanceToolStripMenuItem.Text = "Client Maintenance";
+            // 
+            // employeeMaintenanceToolStripMenuItem
+            // 
+            this.employeeMaintenanceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addEmployeeToolStripMenuItem,
+            this.updateEmployeeToolStripMenuItem,
+            this.removeEmployeeToolStripMenuItem});
+            this.employeeMaintenanceToolStripMenuItem.Name = "employeeMaintenanceToolStripMenuItem";
+            this.employeeMaintenanceToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.employeeMaintenanceToolStripMenuItem.Text = "Employee Maintenance";
+            // 
+            // userMaintenanceToolStripMenuItem
+            // 
+            this.userMaintenanceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addUserToolStripMenuItem,
+            this.updateUserToolStripMenuItem,
+            this.removeUserToolStripMenuItem});
+            this.userMaintenanceToolStripMenuItem.Name = "userMaintenanceToolStripMenuItem";
+            this.userMaintenanceToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.userMaintenanceToolStripMenuItem.Text = "User Maintenance";
+            // 
+            // addClientToolStripMenuItem
+            // 
+            this.addClientToolStripMenuItem.Name = "addClientToolStripMenuItem";
+            this.addClientToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addClientToolStripMenuItem.Text = "Add Client";
+            // 
+            // updateClientToolStripMenuItem
+            // 
+            this.updateClientToolStripMenuItem.Name = "updateClientToolStripMenuItem";
+            this.updateClientToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.updateClientToolStripMenuItem.Text = "Update Client";
+            // 
+            // addEmployeeToolStripMenuItem
+            // 
+            this.addEmployeeToolStripMenuItem.Name = "addEmployeeToolStripMenuItem";
+            this.addEmployeeToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.addEmployeeToolStripMenuItem.Text = "Add Employee";
+            // 
+            // updateEmployeeToolStripMenuItem
+            // 
+            this.updateEmployeeToolStripMenuItem.Name = "updateEmployeeToolStripMenuItem";
+            this.updateEmployeeToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.updateEmployeeToolStripMenuItem.Text = "Update Employee";
+            // 
+            // removeEmployeeToolStripMenuItem
+            // 
+            this.removeEmployeeToolStripMenuItem.Name = "removeEmployeeToolStripMenuItem";
+            this.removeEmployeeToolStripMenuItem.Size = new System.Drawing.Size(172, 22);
+            this.removeEmployeeToolStripMenuItem.Text = "Remove Employee";
+            // 
+            // addUserToolStripMenuItem
+            // 
+            this.addUserToolStripMenuItem.Name = "addUserToolStripMenuItem";
+            this.addUserToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.addUserToolStripMenuItem.Text = "Add User";
+            // 
+            // updateUserToolStripMenuItem
+            // 
+            this.updateUserToolStripMenuItem.Name = "updateUserToolStripMenuItem";
+            this.updateUserToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.updateUserToolStripMenuItem.Text = "Update User";
+            // 
+            // removeUserToolStripMenuItem
+            // 
+            this.removeUserToolStripMenuItem.Name = "removeUserToolStripMenuItem";
+            this.removeUserToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.removeUserToolStripMenuItem.Text = "Remove User";
+            // 
+            // reportsToolStripMenuItem
+            // 
+            this.reportsToolStripMenuItem.Name = "reportsToolStripMenuItem";
+            this.reportsToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.reportsToolStripMenuItem.Text = "Reports";
+            // 
+            // pointsOfSaleToolStripMenuItem
+            // 
+            this.pointsOfSaleToolStripMenuItem.Name = "pointsOfSaleToolStripMenuItem";
+            this.pointsOfSaleToolStripMenuItem.Size = new System.Drawing.Size(198, 22);
+            this.pointsOfSaleToolStripMenuItem.Text = "Points of Sale";
+            // 
+            // logoutToolStripMenuItem
+            // 
+            this.logoutToolStripMenuItem.Name = "logoutToolStripMenuItem";
+            this.logoutToolStripMenuItem.Size = new System.Drawing.Size(57, 20);
+            this.logoutToolStripMenuItem.Text = "Logout";
+            // 
+            // UI_MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1264, 730);
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1280, 768);
+            this.MinimumSize = new System.Drawing.Size(1280, 726);
+            this.Name = "UI_MainWindow";
+            this.ShowIcon = false;
+            this.Text = "UI_MainWindow";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.UI_MainWindow_FormClosing);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clientMaintenanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addClientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateClientToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem employeeMaintenanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addEmployeeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateEmployeeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeEmployeeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem pointsOfSaleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem reportsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem userMaintenanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem updateUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem removeUserToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem logoutToolStripMenuItem;
+    }
+}
