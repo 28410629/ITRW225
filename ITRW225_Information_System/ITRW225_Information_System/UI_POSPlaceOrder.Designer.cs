a76b9d78f59c0836bf63d484183f00ef59a0ae47
Ictinus2310 <coen.human@gmail.com>
2018-09-04 20:03:53 +0200

    Place order nearly done in terms of GUI

--- ITRW225_Information_System/ITRW225_Information_System/UI_POSPlaceOrder.Designer.cs
+++ ITRW225_Information_System/ITRW225_Information_System/UI_POSPlaceOrder.Designer.cs
@@ -67,7 +67,7 @@
             this.listView1.Location = new System.Drawing.Point(350, 63);
             this.listView1.Name = "listView1";
             this.listView1.ShowItemToolTips = true;
-            this.listView1.Size = new System.Drawing.Size(438, 301);
+            this.listView1.Size = new System.Drawing.Size(464, 301);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -79,7 +79,7 @@
             // 
             // columnP
             // 
-            this.columnP.Text = "Selected Product to Add";
+            this.columnP.Text = "Selected Products";
             this.columnP.Width = 259;
             // 
             // textBox1
@@ -147,7 +147,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(826, 450);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
