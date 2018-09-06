deda1aee2371232d1ded8fb4b4e457b72f426cdb
coenraadhuman <coen.human@gmail.com>
2018-09-06 09:03:51 +0200

    POS updates!

--- ITRW225_Information_System/ITRW225_Information_System/UI_POSPlaceOrder.Designer.cs
+++ ITRW225_Information_System/ITRW225_Information_System/UI_POSPlaceOrder.Designer.cs
@@ -46,13 +46,16 @@
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.columnC = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // listView1
             // 
             this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnQ,
-            this.columnP});
+            this.columnP,
+            this.columnC});
             this.listView1.FullRowSelect = true;
             this.listView1.GridLines = true;
             this.listView1.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.Nonclickable;
@@ -60,15 +63,16 @@
             this.listView1.Location = new System.Drawing.Point(350, 12);
             this.listView1.Name = "listView1";
             this.listView1.ShowItemToolTips = true;
-            this.listView1.Size = new System.Drawing.Size(464, 301);
+            this.listView1.Size = new System.Drawing.Size(464, 277);
             this.listView1.TabIndex = 1;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.AfterLabelEdit += new System.Windows.Forms.LabelEditEventHandler(this.listView1_AfterLabelEdit);
             // 
             // columnQ
             // 
             this.columnQ.Text = "Quantity";
-            this.columnQ.Width = 175;
+            this.columnQ.Width = 100;
             // 
             // columnP
             // 
@@ -205,13 +209,29 @@
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(331, 277);
             this.listBox1.TabIndex = 31;
-            this.listBox1.Click += new System.EventHandler(this.listBox1_Click);
+            this.listBox1.DoubleClick += new System.EventHandler(this.listBox1_DoubleClick);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(350, 290);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(464, 23);
+            this.button3.TabIndex = 32;
+            this.button3.Text = "Delete Selected Item";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // columnC
+            // 
+            this.columnC.Text = "Price";
+            this.columnC.Width = 90;
             // 
             // UI_POSPlaceOrder
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(826, 403);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.dateTimePicker1);
@@ -257,5 +277,7 @@
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ColumnHeader columnC;
+        private System.Windows.Forms.Button button3;
     }
 }
