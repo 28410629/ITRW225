98951a42404b60648051779957e82d330db35ff7
Ictinus2310 <coen.human@gmail.com>
2018-09-14 10:53:31 +0200

    Employee Add Works

--- ITRW225_Information_System/ITRW225_Information_System/UI_EmployeeAdd.cs
+++ ITRW225_Information_System/ITRW225_Information_System/UI_EmployeeAdd.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.OleDb;
 using System.Windows.Forms;
 
 namespace ITRW225_Information_System
 {
     public partial class UI_EmployeeAdd : Form
     {
+        private List<string[]> employeeType;
         private List<string[]> idDetails;
         Form mainForm;
         public UI_EmployeeAdd(Form mainForm)
@@ -200,10 +202,32 @@ namespace ITRW225_Information_System
                             MessageBox.Show("Please select appropriate location.");
                             break;
                         default:
-                            BE_DatabaseCommands commands = new BE_DatabaseCommands();
-                            string query = String.Format("");
-                            commands.addDB(query, "CONTACT_DETAILS");
+                            buttonSave.Enabled = false;
+                            // this adds person
+                            using (OleDbConnection db = new OleDbConnection(Properties.Settings.Default.DatabaseConnectionString))
+                            {
+                                string query = String.Format("INSERT INTO PERSON (Person_ID,Person_Name,Person_Surname,Person_Is_Removed,Person_Is_Employee,Person_Type, Person_Added) VALUES('{0}', '{1}', '{2}', False, True, " + (comboBoxPosition.SelectedIndex + 1) + ", @1)", textBoxID.Text, textBoxFN.Text, textBoxLN.Text);
+                                db.Open();
+                                OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM PERSON", db);
+                                OleDbCommand command = new OleDbCommand(query, db);
+                                command.Parameters.Add("@1", OleDbType.Date).Value = DateTime.Today;
+                                adapter.InsertCommand = command;
+                                adapter.InsertCommand.ExecuteNonQuery();
+                                db.Close();
+                            }
+                            // this add contact details
+                            using (OleDbConnection db = new OleDbConnection(Properties.Settings.Default.DatabaseConnectionString))
+                            {
+                                string query = String.Format("INSERT INTO CONTACT_DETAILS (Person_ID, House_Number, Street_Name, Postal_Code, Cell_Number_1, Cell_Number_2, Suburb, City, Email_Address) VALUES('{0}', '{1}', '{2}', {3}, {4}, {5}, '{6}', '{7}', '{8}')", textBoxID.Text, textBoxHN.Text, textBoxSN.Text, textBoxPC.Text, textBoxCN.Text, textBoxCN2.Text, textBoxS.Text, comboBoxCN.SelectedItem.ToString(), textBoxEA.Text);
+                                db.Open();
+                                OleDbDataAdapter adapter = new OleDbDataAdapter("SELECT * FROM PERSON", db);
+                                OleDbCommand command = new OleDbCommand(query, db);
+                                adapter.InsertCommand = command;
+                                adapter.InsertCommand.ExecuteNonQuery();
+                                db.Close();
+                            }
                             MessageBox.Show("Successfully updated database!");
+                            buttonSave.Enabled = true;
                             break;
                     }
                 }
@@ -212,6 +236,7 @@ namespace ITRW225_Information_System
                     BE_LogSystem log = new BE_LogSystem(ex);
                     log.saveError();
                     MessageBox.Show("Failed updating database!");
+                    buttonSave.Enabled = true;
                 }
             }
         }
@@ -221,6 +246,12 @@ namespace ITRW225_Information_System
             buttonClose.Focus();
             BE_DatabaseCommands commands = new BE_DatabaseCommands();
             idDetails = commands.retrieveCustomDB("SELECT * FROM PERSON, CONTACT_DETAILS WHERE PERSON.Person_ID = CONTACT_DETAILS.Person_ID");
+            employeeType = commands.retrieveDB("PERSON_TYPE");
+            for (int i = 0; i < employeeType.Count; i++)
+            {
+                comboBoxPosition.Items.Add(employeeType[i][1]);
+            }
+            comboBoxPosition.SelectedIndex = 0;
         }
 
         private void UI_EmployeeAdd_FormClosing(object sender, FormClosingEventArgs e)
